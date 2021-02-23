b450f18c8285730c147edfc52a7b74b220e97db6
Björn Strömberg <bjorn.stromberg@codic.se>
2021-02-23 16:04:09 +0100

    lite cookie arbete

--- öva_ES6_moduler/js/store.js
+++ öva_ES6_moduler/js/store.js
@@ -2,8 +2,35 @@
 // om cookies https://www.w3schools.com/js/js_cookies.asp
 
 // Steg 1: skriv ett snabbt demo på en testsida för att lista ut hur "cookies" fungerar
+//document.cookie = "myScore=4;";
+//document.cookie = "myUrl=https://cdn2.thecatapi.com/images/507.jpg;";
 
-// Steg 2: steg två skapa hjälpfunktioner (save, load) som kan exporteras bort från modulen
+setCookie("test", "fem");
+console.log(getCookie());
+
+// Steg 2: skapa hjälpfunktioner (save, load) som kan exporteras bort från modulen
+function setCookie(key, value) {
+  document.cookie = `${key}=${value};`;
+}
+
+function getCookie() {
+  let obj = {};
+  //console.log(document.cookie);
+
+  for (const pair of document.cookie.split(";")) {
+    const index = pair.indexOf("=");
+    const key = pair.slice(0, index).trim();
+    const value = pair.slice(index + 1);
+
+    //console.log(`key:${key} value:${value}`);
+
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
+debugger;
 
 // Fantisera lite om hur funktionerna hade kunnat användas, tex. som nedan
 // save("myUrl", userCatUrl);
